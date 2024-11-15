6a569fb6330620f72ada651b044d9567be652c77
Arsen Yeremin <arsen3d@gmail.com>
2024-11-15 13:34:52 -0800

    Update

--- run.sh
+++ run.sh
@@ -20,7 +20,8 @@ done
 # output=$(echo "$(bacalhau docker run --network=Full --entrypoint=./run_app.sh --env \"CALL_BACK=$CALL_BACK\"  arsen3d/gradio_whisper:latest)" | tail -n 1)
 # echo $output
 rm -rf /tmp/lilypad/data/repos/arsenum/gradio_whisper_module
-lilypad run github.com/arsenum/gradio_whisper_module:main --target 0x6519E04dEb2793F07b1b4756c922777F45B78880 --web3-private-key=$WEB3_USER_PRIVATE_KEY -i ENV="CALL_BACK=$CALL_BACK"
+lilypad run github.com/arsenum/gradio_whisper_module:main --target 0xd10D15cc705f7D2558352B1212A9b3685155d93D --web3-private-key=$WEB3_USER_PRIVATE_KEY -i ENV="CALL_BACK=$CALL_BACK"
+# lilypad run github.com/lilypad-tech/lilypad-module-cowsay:main --target 0xd10D15cc705f7D2558352B1212A9b3685155d93D --web3-private-key=$WEB3_USER_PRIVATE_KEY -i ENV="CALL_BACK=$CALL_BACK"
 exit
 # last_line=$(echo "$(output)")
 # echo $("$output")
